54eacc2ad72333c915b293436cf55a7046f9a772
junya-kume <kume.junya@gmail.com>
2021-03-01 16:56:59 +0900

    テーブル作成、バリデーション、ルーティング設定

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence:  true
+  validates :email, presence:  true
+  validates :encryted_password, presence:  true
+  validates :first_name, presence:  true
+  validates :last_name, presence:  true
+  validates :first_furigana, presence:  true
+  validates :last_furigana, presence:  true
+  validates :birthday, presence:  true
+end
+
