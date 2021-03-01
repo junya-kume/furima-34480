74cd4b5affdeccf2e68f9130f4afc88075db6531
junya-kume <kume.junya@gmail.com>
2021-03-01 23:08:42 +0900

    ログイン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  # validates :encrypted_password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_furigana, presence: true
