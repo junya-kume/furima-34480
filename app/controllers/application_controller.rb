449779be69a270e46bc2618e66b934acd073817a
junya-kume <kume.junya@gmail.com>
2021-03-01 22:55:09 +0900

    新規登録実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitred_parameters, if: :devise_controller?
 
   private
-  def configure_permitred_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :email, :encrypted_password, :first_name, :last_name, :first_furigana, :last_furigana, :birthday])
+  def configure_permitred_parameters   
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :email, :first_name, :last_name, :first_furigana, :last_furigana, :birthday])
   end
 
   def basic_auth
