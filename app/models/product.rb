54af37d7c0eb2db6b4dde18d1a29e0e2e8354286
junya-kume <kume.junya@gmail.com>
2021-03-03 16:37:55 +0900

    バリデーションを記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :user
+  with_options presence: true do
+    validates :name
+    validates :descriotion
+    validates :category_id
+    validates :status_id
+    validates :shopping_cost_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+    validates :price
+  end
 end
