9663f6429b57572ec12b96e3da34dff452e057da
junya-kume <kume.junya@gmail.com>
2021-03-03 15:30:19 +0900

    Rspecの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,24 @@ class User < ApplicationRecord
   
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_furigana, presence: true
-  validates :last_furigana, presence: true
-  validates :birthday, presence: true
+  NAME_REGEX = /\A(?=.*?[ぁ-んァ-ヶ一-龥々])[ぁ-んァ-ヶ一-龥々]+\z/i.freeze
+  FURIGANA_REGEX = /\A(?=.*?[ァ-ヶ一])[ァ-ヶ一]+\z/i.freeze
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :birthday
+    with_options format:{ with: PASSWORD_REGEX, message: 'Include both letters and numbers'} do
+      validates :password
+    end
+    with_options format:{ with: NAME_REGEX, message: '全角漢字・ひらがな・カタカナ以外は登録できません'} do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format:{ with: FURIGANA_REGEX, message: 'カタカナ以外は登録できません'} do
+      validates :first_furigana
+      validates :last_furigana
+    end
+  end
 end
 
 
