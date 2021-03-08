98bc8ee2d15c0439bed46326192a04f9d320b9ad
junya-kume <kume.junya@gmail.com>
2021-03-08 13:05:43 +0900

    コントローラー、ビューの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :product_find, only:[:show, :edit, :update]
+  before_action :user_check, only:[:edit, :update]
   def index
     @products = Product.all.order("created_at DESC")
     #@user_products = UserProduct.all
@@ -19,18 +21,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
-    unless (current_user.id == @product.user_id)
-      redirect_to action: :index
-    end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path
     else
@@ -41,5 +37,15 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :status_id, :shopping_cost_id, :shipping_area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def product_find
+    @product = Product.find(params[:id])
+  end
+  
+  def user_check
+    unless (current_user.id == @product.user_id)
+      redirect_to action: :index
+    end
   end
 end
