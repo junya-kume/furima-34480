6b1040ab0f92bc369f8d314c734a89c604c95633
junya-kume <kume.junya@gmail.com>
2021-03-12 12:31:21 +0900

    修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -33,11 +33,11 @@ class PurchasesController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: @product.price,  # 商品の値段
-      card: purchase_product_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @product.price,
+      card: purchase_product_params[:token],
+      currency: 'jpy'
     )
   end
 end
